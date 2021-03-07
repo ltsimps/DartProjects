29b4655b0c3b4b70db2da4f8e9f83e09a9b0c1f8
ltsimps <ltsimpso@me.com>
2021-03-07 11:26:45 -0500

    AI picking added

--- RockPaperScissors/rock_paper_scissors.dart
+++ RockPaperScissors/rock_paper_scissors.dart
@@ -1,14 +1,28 @@
 import 'dart:io';
 
+import 'dart:math';
+
+enum Pick { rock, paper, scissors }
+
 void main() {
+  final rng = Random();
+
   while (true) {
     stdout.write("Rock, paper, or scissors? (r/p/s) ");
-    //enum game = {rock, paper, scissors};
 
     final input = stdin.readLineSync();
-    if (input == 'r' || input == 'p' || input == 's')
-      print('Selected $input');
-    else if (input == 'q')
+    if (input == 'r' || input == 'p' || input == 's') {
+      //print('Selected $input');
+      var playerMove = null;
+      if (input == 'r')
+        playerMove = Pick.rock;
+      else if (input == 's')
+        playerMove = Pick.scissors;
+      else if (input == 'p') playerMove = Pick.paper;
+      print("player move is $playerMove");
+      final AiMove = rng.nextInt(3);
+      print("player move is ${Pick.values[AiMove]}");
+    } else if (input == 'q')
       break;
     else
       print('You\'ve entered invalid input');
